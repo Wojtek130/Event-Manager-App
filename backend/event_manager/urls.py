365c85d9dc61a3f11f93e7d8a64f2c5ca9bbb2d6
Wojtek130 <pimpek86@gmail.com>
2023-12-02 22:14:43 +0100

    authentication in backend without access tokens implemented

--- backend/event_manager/urls.py
+++ backend/event_manager/urls.py
@@ -1,8 +1,12 @@
 # yourappname/urls.py
 
 from django.urls import path
-from .views import index
+from .views import index, index_protected, register_user, login_user, logout_user
 
 urlpatterns = [
     path('', index, name='index'),
+    path('protected', index_protected, name='index_protected'),
+    path('auth/register/', register_user, name='register'),
+    path('auth/login/', login_user, name='login'),
+    path('auth/logout/', logout_user, name='logout'),
 ]
--- backend/event_manager/views.py
+++ backend/event_manager/views.py
@@ -1,6 +1,45 @@
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 from django.http import JsonResponse
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny, IsAuthenticated
+
+from .serializers import MyUserSerializer
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def register_user(request):
+    user = MyUserSerializer(data=request.data)
+    if user.is_valid():
+        user.save()
+        return Response(user.data, status=status.HTTP_201_CREATED)
+    return Response(user.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def login_user(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+    print(username, password)
+    user = authenticate(request, username=username, password=password)
+    if user:
+        login(request, user)
+        return Response({'message': 'Login successful'}, status=status.HTTP_200_OK)
+    return Response({'message': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def logout_user(request):
+    logout(request)
+    return Response({'message': 'Logout successful'}, status=status.HTTP_200_OK)
 
 def index(request):
     return JsonResponse(data={"hello" : "world"})
+
+@permission_classes([IsAuthenticated])
+def index_protected(request):
+    return JsonResponse(data={"hello" : "protected"})
 
 
