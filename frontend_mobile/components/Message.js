9cd96324c14a4bfb041feb9a144a9939da903dc2
Wojtek130 <pimpek86@gmail.com>
2024-01-05 14:49:22 +0100

    messages styles

--- frontend_mobile/components/Message.js
+++ frontend_mobile/components/Message.js
@@ -1,7 +1,22 @@
-import { View, Text, StyleSheet } from "react-native";
+import { Text, StyleSheet } from "react-native";
+
+import {
+  bluePrimary,
+  borderRadius,
+} from "../utils/stylesConstants";
 
 const Message = (props) => {
-  return <Text>{props.item.body}</Text>;
+  return <Text style={[styles.text]}>{props.item.body}</Text>;
 };
 
+const styles = StyleSheet.create({
+  text: {
+    backgroundColor: "white",
+    color: bluePrimary,
+    paddingVertical: 2,
+    paddingHorizontal: 7,
+    margin: 2,
+    borderRadius: borderRadius,
+  },
+});
 export default Message;
