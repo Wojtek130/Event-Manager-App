f0e9e25c9b4c47a2ae069e1fa37f4c24b9f4b294
Wojtek130 <pimpek86@gmail.com>
2024-01-26 13:20:00 +0100

    announcement list file cleaned up

--- frontend_mobile/screens/AnnouncementsListScreen.js
+++ frontend_mobile/screens/AnnouncementsListScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, FlatList } from "react-native";
+import { View, Text, } from "react-native";
 import { useSelector } from "react-redux";
 
 import ErrorMessage from "../components/ErrorMessage";
@@ -33,20 +33,9 @@ const AnnouncementsListScreen = ({ navigation }) => {
       const fetchData = async () => {
         try {
           const response = await axiosInstance.get("/events");
-          // const m = [
-          //   {
-          //     name: "aaaa123",
-          //     id: 1,
-          //     am_organizer: true,
-          //     am_participant: false,
-          //   },
-          // ];
           const filteredEvent = response.data.events.filter(
             (item) => item.am_organizer || item.am_participant
           );
-          console.log(response.data.events, "mmmmmmmmmmmm");
-          // setEvents(response.data.events);
-          // setEventsDisplayed(response.data.events);
           setEvents(filteredEvent);
           setEventsDisplayed(filteredEvent);
         } catch (error) {
@@ -75,29 +64,6 @@ const AnnouncementsListScreen = ({ navigation }) => {
       <MyFlatList
         data={eventsDisplayed}
         renderItem={({ item, index, separators }) => {
-          // console.log(item, index, separators);
-          // if (item.am_organizer || item.am_participant) {
-          // return (
-          //   <>
-          //     <Text>{item.name} ?</Text>
-          //   </>
-          // );
-          //   return (
-          //     <ListItem
-          //       item={item}
-          //       unreadMessagesAvailable={unreadMessagesAvailable(
-          //         item,
-          //         newMessages
-          //       )}
-          //       onPress={() => handleEventPress(item)}
-          //       isChatHeader={true}
-          //     />
-          //   );
-          // } else {
-          //   return <Text>a</Text>;
-          //   // return null;
-
-          // }
           if (item.am_organizer || item.am_participant) {
             return (
               <ListItem
